1be5b8093eff60e7e89be0e84bddf7d58514d21f
Fay Vera <fvera.cavalcante@gmail.com>
2021-04-27 16:40:35 -0400

    commit tree 9e3da0d04b9e8de22e464b91c51fc203546cf1c1

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
 class Formatter {
   //add static methods here
+  static capitalize(word){
+    return word.charAt(0).toUpperCase() + word.slice(1)
+  }
+
+  static sanitize(string){
+    return string.replace( /[^A-Za-z0-9 '-]+/g, '')
+  }
+
+  static titleize(string){
+    let exceptions = [ 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ]
+    let arr = []
+
+    let words = string.split(' ') 
+
+    for (let i = 0; i < words.length; i++){
+      if(i == 0){
+        arr.push(this.capitalize(words[i]))
+      } else {
+        if (exceptions.includes(words[i])){
+          arr.push(words[i])
+        } else {
+          arr.push(this.capitalize(words[i]))
+        }
+      }
+    }
+    return arr.join(' ');
+  }
+
+
+
 }
